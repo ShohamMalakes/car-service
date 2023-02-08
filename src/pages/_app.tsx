447e003c140f321d764b1e5696c0976a47a9d81e
ShohamMalakes <shoam233@gmail.com>
2023-02-08 16:45:50 +0200

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
-import { type AppType } from "next/app";
+import { AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import { api } from "../utils/api";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,12 @@
+import styles from "./index.module.css";
 import { type NextPage } from "next";
-import { useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
 
 import { api } from "../utils/api";
 
 const Home: NextPage = () => {
-  const data = api.vehicle.getData.useQuery({ license_plate: "4430086" });
-
-  useEffect(() => {
-    console.log(data.data);
-  }, [data]);
-
-  return (
-    <>
-      <button>call me</button>
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
-
